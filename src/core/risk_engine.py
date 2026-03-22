aeb208e32fc0e0cb5bf6d79ea3480a53e7e09a84
d07056549-bit <d07056549@gmail.com>
2026-03-22 00:35:50 +0000

    Update risk_engine.py

--- src/core/risk_engine.py
+++ src/core/risk_engine.py
@@ -3,11 +3,22 @@ class SofieRiskEngine:
         self.data = data
 
     def calculate_global_fragility(self):
-        """Math to determine if the world is entering a Black Swan event."""
-        # S^3 Formula: (Price Shock * Risk Index) + Shipping Friction
-        oil_shock = (self.data['oil_price'] - 75) / 75
-        friction = self.data['shipping_delay'] * 2
+        """
+        The 2026 Fragility Formula:
+        High Score = High Risk.
+        """
+        # 1. Energy Shock (How much higher than 'normal' $75 oil?)
+        oil_impact = (self.data['oil_price'] - 75) / 75 * 30
         
-        # Stability Score (0 = Perfect, 100 = Total Collapse)
-        score = (oil_shock * 40) + (self.data['gpr_index'] / 10) + (friction * 10)
-        return round(score, 2)
+        # 2. Geopolitical Tension (GPR Index / 10)
+        tension_impact = self.data['gpr_index'] / 10
+        
+        # 3. Financial Contagion (Number of 'B/C' rated countries * 1.5)
+        # This is the "Sovereign Risk" weight
+        contagion_impact = self.data['sovereign_risk_entities'] * 1.5
+        
+        # Total Score
+        final_score = oil_impact + tension_impact + contagion_impact
+        
+        # Return as a clean number out of 100 (capped)
+        return min(round(final_score, 2), 100.0)
